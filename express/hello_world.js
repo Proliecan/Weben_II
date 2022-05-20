454690e2105b08e13942a318dd4ea99154e5a083
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2022-05-20 22:46:15 +0200

    Express PUT request echo

--- express/hello_world.js
+++ express/hello_world.js
@@ -7,6 +7,20 @@ app.get('/', (req, res)=>{
     return res.send("Hello World!")
 })
 
+app.put("/user", (req, res)=>{
+    let chunks = []
+    let temp
+    let data
+    req.on("data", (chunk => {
+        chunks.push(chunk)
+    }))
+    req.on("end", () =>{
+        temp = Buffer.concat(chunks)
+        data = JSON.parse(temp)
+        res.json(data)
+    })
+})
+
 app.listen(port, /*hostname,*/ ()=>{
     console.log(`Server started on port ${port}` /* with hostname ${hostname} */)
 })
